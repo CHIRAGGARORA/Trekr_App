ee11c76c6cd2fc8cee42bfe4392fc98950a5af61
Chirag Arora <arora2002chirag@gmail.com>
2022-12-20 18:58:58 +0530

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by chirag arora on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
